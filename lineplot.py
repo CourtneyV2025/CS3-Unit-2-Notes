13ffc2daaf4ab89a74647b464a819c85c1100a6c
CourtneyV2025 <courtney.valis2025@gbwl.org>
2024-10-22 19:11:28 +0000

    lineplot

--- lineplot.py
+++ lineplot.py
@@ -23,9 +23,35 @@ plt.savefig('lineplots.png')
 fig = plt.figure()
 ax = plt.axes() 
 
+# LINEPLOT TWO
+
 # Plot a function on the Axes object instance 
 ax.plot(x_vals, 2*(x_vals), color='pink')
+ax.plot(x_vals, 3*(x_vals),linestyle='dashed')
+ax.plot(x_vals, 4*(x_vals), linestyle='dashdot')
+ax.plot(x_vals, 5*(x_vals), linestyle='dotted') 
 
+# You can use shortcuts to define color & style
+ax.plot(x_vals, 1.5*(x_vals), ':')
+
+# Save figure as PNG in current directory
 fig.savefig('lineplot2.png')
+plt.close()
 
+# LINEPLOT THREE
 
+# Demo in customization 
+plt.plot(x_vals, np.sin(x_vals), '-.m', label='sin(x)')
+plt.xlim(-1,11)
+plt.ylim(-1.5, 1.5) 
+# Set axis to tight to fit your plot better
+plt.axis('tight')
+# other options: 'equal', 'image', etc.
+# Add TITLES/LABELS
+plt.title('A Sine Wave') 
+plt.xlabel('x')
+plt.ylabel('sin(x)')
+
+# Add LEGEND
+plt.legend()
+plt.savefig('lineplot3.png')
